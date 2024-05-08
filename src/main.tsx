102b9a9e51a95ae1a5030da65d6727b29236b3b0
alexugm <aguerrero@anzarenewables.com>
2024-05-08 17:53:25 -0600

    add pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import SignoutPage from './pages/SignoutPage';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/sign-out",
+    element: <SignoutPage />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
